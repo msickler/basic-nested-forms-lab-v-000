71d19c3fc9b27ce6bab691f1d0016b84afc5d44a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 18:35:28 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,8 +10,7 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @recipe.ingredients.build
-    @recipe.ingredients.build
-    
+    @recipe.ingredients.build    
   end
 
   def create
@@ -22,13 +21,7 @@ class RecipesController < ApplicationController
   private 
 
   def recipe_params
-    params.require(:recipe).permit(
-    :ingredients, 
-    ingredients_attributes: [
-      :name,
-      :quantity
-    ]
-    )
+    params.require(:recipe).(:title, ingredients_attributes: [:name, :quantity])
   end
   
 end
